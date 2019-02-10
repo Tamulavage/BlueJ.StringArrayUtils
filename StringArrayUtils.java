614ed935f44bb559cb3dfc0a4bc6b5af91ce42dd
David Tamulavage <davidtamulavage@zipcoders-MBP.fios-router.home>
2019-02-09 21:05:28 -0500

    fixed functions, including dup element strings

--- StringArrayUtils.java
+++ StringArrayUtils.java
@@ -3,13 +3,17 @@
 /**
  * Created by leon on 1/29/18.
  */
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
 public class StringArrayUtils {
     /**
      * @param array array of String objects
      * @return first element of specified array
      */ // TODO
     public static String getFirstElement(String[] array) {
-        return null;
+        return array[0];
     }
 
     /**
@@ -17,7 +21,7 @@ public class StringArrayUtils {
      * @return second element in specified array
      */
     public static String getSecondElement(String[] array) {
-        return null;
+        return array[1];
     }
 
     /**
@@ -25,7 +29,7 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+        return array[array.length-1];
     }
 
     /**
@@ -33,7 +37,7 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+        return array[array.length-2];
     }
 
     /**
@@ -42,7 +46,16 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
-        return false;
+        boolean retVal = false;
+        
+        
+        for(int i=0; i<array.length && retVal == false ;i++)
+        {
+            retVal = value.equals(array[i]);
+        }
+        
+        // looks like there is an array util for the contains . But wanted to try with out using it
+        return retVal;
     }
 
     /**
@@ -50,7 +63,20 @@ public class StringArrayUtils {
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+        
+        String arrayHold[] = new String[array.length];
+        int arrayLen = array.length;
+        
+        for(int i=0; i<array.length;i++)
+        {
+            
+            arrayHold[arrayLen-1] = array[i]; // 2 .. 0, 1..1 , 0 .. 2, 
+            arrayLen--;
+        }
+        
+      
+        // there is a array String.Reverse, but wanted to figure it out with out using it
+        return arrayHold;
     }
 
     /**
@@ -58,7 +84,7 @@ public class StringArrayUtils {
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static boolean isPalindromic(String[] array) {
-        return false;
+        return Arrays.equals(array, reverse(array));
     }
 
     /**
@@ -66,7 +92,40 @@ public class StringArrayUtils {
      * @return true if each letter in the alphabet has been used in the array
      */ // TODO
     public static boolean isPangramic(String[] array) {
-        return false;
+        // concate the values to 1 string, then check the string
+        String nonArrayString = "";
+        boolean retVal = true;
+        int charLength=0;
+        char  tempHold;
+       
+        
+        // put into string just to get the overall count
+        for(int i=0;i<array.length;i++) {
+            
+            nonArrayString = nonArrayString + array[i];
+        }
+        
+        //Move back into a string array of individual strings
+        String newArray[] = new String[nonArrayString.length()];
+        for(int b =0; b <nonArrayString.length() ; b++ ){
+            tempHold= nonArrayString.charAt(b);
+            newArray[b] = Character.toString(tempHold);
+        }
+        
+        
+        // now that it is 1 string - which is an array of chars, loop through single array
+        for(Character value = 'a'; value < 'z'; value++) {
+            //System.out.println((contains(newArray,value.toString())||contains(newArray,value.toString().toUpperCase())));
+            if(!(contains(newArray,value.toString())||contains(newArray,value.toString().toUpperCase())))
+            {
+                retVal = false;
+                //System.out.println("Not Found");
+            }
+            
+        }
+        
+       
+        return retVal;
     }
 
     /**
@@ -75,7 +134,18 @@ public class StringArrayUtils {
      * @return number of occurrences the specified `value` has occurred
      */ // TODO
     public static int getNumberOfOccurrences(String[] array, String value) {
-        return 0;
+         int retVal = 0;
+        
+        
+        for(int i=0; i<array.length ; i++)
+        {
+            if(value.equals(array[i])) {
+                retVal++;
+            }
+        }
+        
+        // looks like there is an array util for the contains . But wanted to try with out using it
+        return retVal;
     }
 
     /**
@@ -84,7 +154,16 @@ public class StringArrayUtils {
      * @return array with identical contents excluding values of `value`
      */ // TODO
     public static String[] removeValue(String[] array, String valueToRemove) {
-        return null;
+        String retArray[] = new String[array.length-1];
+        
+        for(int n=0, i=0; i<array.length  ;i++, n++)
+        { 
+             if(valueToRemove.equals(array[i])){
+                  i++;
+                }
+                retArray[n] = array[i];
+        }
+          return retArray;
     }
 
     /**
@@ -92,7 +171,43 @@ public class StringArrayUtils {
      * @return array of Strings with consecutive duplicates removes
      */ // TODO
     public static String[] removeConsecutiveDuplicates(String[] array) {
-        return null;
+        
+        
+         int numOfDups=0;
+         int newArrayIndex=0;
+         String prev=new String();
+        // System.out.println("------");
+         
+         // get new size first
+         for(int i=0; i<(array.length-1); i++){
+            if(array[i].equals(array[i+1])){
+                numOfDups++;
+              //  System.out.println(array[i] + " " + array[i+1]);
+            }
+            
+             //System.out.println(numOfDups );
+        }
+         
+         String retArray[] = new String[array.length-numOfDups];
+         
+        for(int i=0; i<(array.length-1); i++){
+            
+                   
+             if(array[i].equals(array[i+1])) {
+                 
+                }
+                else {
+                 retArray[newArrayIndex] = array[i];
+                 newArrayIndex++;
+                 System.out.println(array[i] + " " + i );
+                }
+     
+              
+            }
+            retArray[newArrayIndex] = array[array.length-1];
+            
+            
+            return retArray;
     }
 
     /**
@@ -100,7 +215,36 @@ public class StringArrayUtils {
      * @return array of Strings with each consecutive duplicate occurrence concatenated as a single string in an array of Strings
      */ // TODO
     public static String[] packConsecutiveDuplicates(String[] array) {
-        return null;
+        
+        //int n = 1;
+        ArrayList<String> concatDups = new ArrayList<String>();
+        String dupString = new String();
+        
+       // System.out.println("-----");
+        
+        for(int i=0;i<array.length;i++) // Loop through entire array List
+    {
+            if(i + 1 != array.length) { // if not out of bounds
+                String next = array[i];
+                while(array[i].equals(array[i+1])){ //  Loop to check for next val equals
+                    next += array[i];
+                    i++;
+                    if (i + 1 == array.length) // check again for out of bounds
+                    {break;
+                    }
+                 }
+                concatDups.add(next); // concat to string element
+            } else {
+                concatDups.add(array[i]); // add string element to array list
+            }
+        
+    }
+       
+        String[] retVal = new String[concatDups.size()]; //new concatDups.size();
+        
+        concatDups.toArray(retVal);
+        
+        return retVal;
     }
 
 
